53c23de361557c0f02792242abf50da923019cf1
yspark <ysparknanum@gmail.com>
2025-05-12 23:39:43 +0900

    update: mcp tool 경로 변경

---
+++ upbit/app/main.py
@@ -0,0 +1,19 @@
+import os
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+
+from app.tools.upbit import set_tools as set_upbit_tools
+
+# load .env file
+load_dotenv()
+
+mcp = FastMCP("Cashy", stateless_http=True)
+set_upbit_tools(mcp)
+
+if __name__ == "__main__":
+    mode = os.getenv("MODE", "stdio")
+
+    if mode == "stdio":
+        mcp.run(transport="stdio")
+    else:
+        mcp.run(transport="streamable-http")
--- upbit/app/tools/upbit.py
+++ upbit/app/tools/upbit.py
@@ -1,4 +1,5 @@
 from httpx import AsyncClient
+from mcp.server.fastmcp import FastMCP
 from dataclasses import dataclass
 from typing import List, Optional
 from datetime import datetime
@@ -7,7 +8,7 @@ from datetime import datetime
 from app.schemas.candle import MinuteCandleStick, DailyCandleStick, WeeklyCandleStick
 from app.schemas.ticker import Ticker
 
-async def get_current_ticker() -> Ticker:
+async def _get_current_ticker() -> Ticker:
     """
     Get the current ticker of Bitcoin in KRW from Upbit API.
     """
@@ -18,7 +19,7 @@ async def get_current_ticker() -> Ticker:
             return Ticker.from_dict(data[0])
         raise ValueError("Bitcoin 티커 정보를 가져올 수 없습니다")
 
-async def get_candles_for_minutes(minutes: int = 30, count: int = 10) -> List[MinuteCandleStick]:
+async def _get_candles_for_minutes(minutes: int = 30, count: int = 10) -> List[MinuteCandleStick]:
     """
     Get a list of minute candlesticks for Bitcoin in KRW from Upbit API.
     This function fetches the candlestick data for the specified minute interval and count.
@@ -42,7 +43,7 @@ async def get_candles_for_minutes(minutes: int = 30, count: int = 10) -> List[Mi
         
         return [MinuteCandleStick.from_dict(item) for item in data]
 
-async def get_candles_for_daily(count: int = 10) -> List[DailyCandleStick]:
+async def _get_candles_for_daily(count: int = 10) -> List[DailyCandleStick]:
     """
     Get a list of daily candlesticks for Bitcoin in KRW from Upbit API.
     Get daily candlestick data until today.
@@ -65,7 +66,7 @@ async def get_candles_for_daily(count: int = 10) -> List[DailyCandleStick]:
         
         return [DailyCandleStick.from_dict(item) for item in data]
 
-async def get_candles_for_weekly(count: int = 10) -> List[WeeklyCandleStick]:
+async def _get_candles_for_weekly(count: int = 10) -> List[WeeklyCandleStick]:
     """
     Get a list of weekly candlesticks for Bitcoin in KRW from Upbit API.
     Get weekly candlestick data until today.
@@ -87,3 +88,15 @@ async def get_candles_for_weekly(count: int = 10) -> List[WeeklyCandleStick]:
         data = response.json()
         
         return [WeeklyCandleStick.from_dict(item) for item in data]
+
+def set_tools(mcp: FastMCP):
+    """
+    Set tools for FastMCP.
+    
+    Args:
+        mcp: FastMCP instance
+    """
+    mcp.add_tool(_get_current_ticker, "get_current_ticker")
+    mcp.add_tool(_get_candles_for_daily, "get_candles_for_daily")
+    mcp.add_tool(_get_candles_for_weekly, "get_candles_for_weekly")
+    mcp.add_tool(_get_candles_for_minutes, "get_candles_for_minutes")
